c3b83588d85cb0218674d7140242b1c22b363d5f
Manuel Rua <manu.rua.123@gmail.com>
2021-03-01 23:12:17 -0300

    barra navegacion
    Actualice la barra de navegacion utilizando bootstrap

--- myapp/src/components/NavBar.js
+++ myapp/src/components/NavBar.js
@@ -1,11 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
 function NavBar() {
     return (
-        <ul>
-        <li><a href="default.asp">Home</a></li>
-        <li><a href="news.asp">News</a></li>
-        <li><a href="contact.asp">Contact</a></li>
-        <li><a href="about.asp">About</a></li>
-      </ul> 
+
+        
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">TITULO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="https://es.wikipedia.org/wiki/Waluigi" target="_blank">Waluigi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://es.wikipedia.org/wiki/Wario" target="_blank">Wario</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://es.wikipedia.org/wiki/Bowser" target="_blank">Bowser</a>
+            </li>
+        </ul>
+        </div>
+        </div>
+    </nav>
+    
+    
+    
+    
     )
 }
 
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import NavBar from './components/NavBar.js'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    
     <NavBar />
   </React.StrictMode>,
   document.getElementById('root')
